3ff66131e0783a1b7be65d8a8957102b3b4ae2da
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-04-03 10:25:17 -0300

    added some methods

--- lib/integrator/model/academic_data.rb
+++ lib/integrator/model/academic_data.rb
@@ -1,9 +1,17 @@
 module Integrator
   class AcademicData < Nested
-    attr_writer :id, :person_id, :career_programme_id, :file_number, :average, :approved_subjects_count, :approved_subjects_active, :last_year_approved_subjects_count, :last_year_active_and_approved_subjects_count, :start_year, :username, :password, :status_id, :career_programme, :status
+    attr_writer :id, :person_id, :career_programme_id, :file_number, :average, :approved_subjects_count, :approved_subjects_active, :last_year_approved_subjects_count, :last_year_active_and_approved_subjects_count, :start_year, :username, :password, :status_id
     
     def to_s
       "#{career_programme_id} [Legajo: #{file_number}]"
+    end
+    
+    def person
+      Person.find(person_id)
+    end
+    
+    def career_programme
+      CareerProgramme.find(career_programme_id)
     end
   end
 end
