288577e295f1cf49f639f0eb761116adab0262d7
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-04-03 10:35:08 -0300

    fixed attr_accessor

--- lib/integrator/model/academic_data.rb
+++ lib/integrator/model/academic_data.rb
@@ -1,9 +1,9 @@
 module Integrator
   class AcademicData < Nested
-    attr_writer :id, :person_id, :career_programme_id, :file_number, :average, :approved_subjects_count, :approved_subjects_active, :last_year_approved_subjects_count, :last_year_active_and_approved_subjects_count, :start_year, :username, :password, :status_id
+    attr_accessor :id, :person_id, :career_programme_id, :file_number, :average, :approved_subjects_count, :approved_subjects_active, :last_year_approved_subjects_count, :last_year_active_and_approved_subjects_count, :start_year, :username, :password, :status_id
     
     def to_s
-      "#{career_programme_id} [Legajo: #{file_number}]"
+      "#{career_programme} [Legajo: #{file_number}]"
     end
     
     def person
