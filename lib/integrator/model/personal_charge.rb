514bb0cdd4fecd0da8bb1ca31a144d8b29250e19
María Emilia Corrons <ecorrons@cespi.unlp.edu.ar>
2015-03-13 10:29:10 -0300

    change parent class

--- lib/integrator/model/personal_charge.rb
+++ lib/integrator/model/personal_charge.rb
@@ -1,6 +1,6 @@
 module Integrator
-  class PersonalCharge < Nested
-    attr_accessor :id, :official_scale, :scale
+  class PersonalCharge < Base
+    attr_reader :id, :official_scale, :scale
 
     def to_s
       "#{scale}"
