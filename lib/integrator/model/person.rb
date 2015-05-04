545c8bc294864de095eade0cb572ef361e71868d
Nahuel Cuesta Luengo <ncuesta@cespi.unlp.edu.ar>
2015-05-04 14:34:52 -0300

    Fixes error in Person#personal_charges

--- lib/integrator/model/person.rb
+++ lib/integrator/model/person.rb
@@ -67,22 +67,9 @@ module Integrator
     end
 
     def personal_charges
-      url = Integrator.url + "/api/person/#{id}/personal_charge.json?token=#{Integrator.token}"
-      url = URI.parse url
-
-      begin
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = (url.scheme == 'https')
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        request = Net::HTTP::Get.new("#{url.path}?#{url.query}")
-        response = http.request(request)
-      rescue Exception => error
-        raise ServerError.new("Could not establish connection: #{error.message}")
+      Client.get(subject: [self, PersonalCharge]).map do |item|
+        PersonalCharge.new(item)
       end
-      
-      raise ServerError.new("Could not establish connection. Message: #{response.message}") if !response.is_a?(Net::HTTPSuccess)
-      
-      ActiveSupport::JSON.decode(response.body)
     end
   end
 end
