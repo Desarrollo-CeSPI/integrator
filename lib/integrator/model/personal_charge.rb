f94c3bdaf08fd36352c93d9acac2a242eb1ca204
Nahuel Cuesta Luengo <ncuesta@cespi.unlp.edu.ar>
2015-05-04 14:36:52 -0300

    Adds #active? and #active_at? to PersonalCharge

--- lib/integrator/model/personal_charge.rb
+++ lib/integrator/model/personal_charge.rb
@@ -1,9 +1,17 @@
 module Integrator
   class PersonalCharge < Base
-    attr_reader :id, :official_scale, :scale
+    attr_reader :id, :official_scale, :scale, :starts_at, :ends_at
 
     def to_s
       "#{scale}"
+    end
+
+    def active?
+      active_at? Date.today
+    end
+
+    def active_at?(date)
+      starts_at.to_date <= date && ends_at.to_date >= date
     end
   end
 end
