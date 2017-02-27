b426e3a489fe692ee0c30c933ade11707770a628
Miguel Carbone <carbomiguel@gmail.com>
2017-02-26 22:23:47 -0300

    V2 - debug

--- lib/integrator/client.rb
+++ lib/integrator/client.rb
@@ -20,6 +20,7 @@ module Integrator
       def build_uri(params = {})
         ensure_subject(params)
         Integrator.url + '/' + slices_from_subject(params[:subject]) + build_params(params)
+        throw Integrator.url + '/' + slices_from_subject(params[:subject]) + build_params(params)
       end
 
       def build_search_uri(params = {})
